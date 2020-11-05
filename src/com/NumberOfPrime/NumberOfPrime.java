49e2a5321212f8e5f2e2c531600086ff7c06af43
mrbeonghia <ntnghia199x@gmail.com>
2020-11-05 11:13:20 +0700

    n so nguyen to dau

---
+++ src/com/NumberOfPrime/NumberOfPrime.java
@@ -0,0 +1,34 @@
+package com.NumberOfPrime;
+
+import java.util.Scanner;
+
+public class NumberOfPrime {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập n = ");
+        int n = scanner.nextInt();
+        System.out.printf("%d số nguyên tố đầu tiên là: \n", n);
+        int count = 0;
+        int i = 2;
+        while (count < n) {
+            if (isPrimeNumber(i)) {
+                System.out.print(i + " ");
+                count++;
+            }
+            i++;
+        }
+    }
+
+    public static boolean isPrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
